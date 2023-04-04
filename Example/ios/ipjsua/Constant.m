a59de081fcc39a6bbff5e01a811e0762b733fe68
Raju Lonka <raju@linkedphone.com>
2023-04-04 18:54:29 +0530

    added PJSUA2 code

---
+++ Example/ios/ipjsua/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  ipjsua
+//
+//  Created by LinkedPhone on 03/04/23.
+//  Copyright © 2023 Teluu. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
