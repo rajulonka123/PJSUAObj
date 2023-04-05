64231d4712085f57736b5cea026f848062638151
Raju Lonka <raju@linkedphone.com>
2023-04-05 13:01:35 +0530

    Added PJSUA2 files

---
+++ Example/PJSUA_pod/ios/ipjsua/Constant.h
@@ -0,0 +1,17 @@
+//
+//  Constant.h
+//  ipjsua
+//
+//  Created by LinkedPhone on 03/04/23.
+//  Copyright © 2023 Teluu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Constant : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
